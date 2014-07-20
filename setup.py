1ac78267c3a45ce4d0d93278871040881b371e77
Graham Higgins <gjh@bel-epa.com>
2014-07-20 03:44:37 +0100

    Migrate to qubit

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 from distutils.core import setup, Extension
 
-xcoin_hash_module = Extension('xcoin_hash',
-                               sources = ['xcoinmodule.c',
-                                          'xcoin.c',
-										  'sha3/blake.c',
-										  'sha3/bmw.c',
-										  'sha3/groestl.c',
-										  'sha3/jh.c',
-										  'sha3/keccak.c',
-										  'sha3/skein.c',
+qubit_hash_module = Extension('qubit_hash',
+                               sources = ['qubitmodule.c',
+                                          'qubit.c',
 										  'sha3/cubehash.c',
 										  'sha3/echo.c',
 										  'sha3/luffa.c',
@@ -16,7 +10,7 @@ xcoin_hash_module = Extension('xcoin_hash',
 										  'sha3/shavite.c'],
                                include_dirs=['.', './sha3'])
 
-setup (name = 'xcoin_hashs',
+setup (name = 'qubit_hash',
        version = '1.0',
-       description = 'Bindings for proof of work used by Xcoin',
-       ext_modules = [xcoin_hash_module])
+       description = 'Bindings for proof of work used by QubitCoin',
+       ext_modules = [qubit_hash_module])
