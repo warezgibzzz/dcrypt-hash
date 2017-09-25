43d49b483f417938d4795f6b00c903a437b5e238
Gibz <me@gibzzz.tk>
2017-09-25 22:51:21 +0700

    ssl hell

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ dcrypt_hash_module = Extension(
     'dcrypt_hash',
     sources=['dcrypt.cpp', 'sha256.cpp', 'uint256.cpp'],
     include_dirs=['.'],
-    extra_compile_args=['-static'],
-    libraries=['crypto', 'boost_system'])
+    extra_compile_args=['-lcrypto', '-lssl'],
+    libraries=['crypto', 'boost_system', 'ssl'])
 
 setup(name='dcrypt_hash',
       version='1.0',
